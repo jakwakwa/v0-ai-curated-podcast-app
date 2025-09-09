876f3885b48545b7cf6ecc37168a9e07964fa80f
Jaco Kotzee <jakwakwa@gmail.com>
2025-09-10 00:25:46 +0200

    style: Refine UI components and global styles for enhanced user experience
    - Updated global styles for card and dialog components to improve visual consistency and responsiveness.
    - Enhanced the layout of the BundleSelectionDialog for better clarity in user interactions.
    - Adjusted spacing and background properties in CuratedBundlesClient for a more cohesive design.
    - Improved dialog styles for better readability and user engagement.

--- app/(protected)/curated-bundles/_components/curated-bundles-client.tsx
+++ app/(protected)/curated-bundles/_components/curated-bundles-client.tsx
@@ -141,14 +141,14 @@ export function CuratedBundlesClient({ bundles, error }: CuratedBundlesClientPro
 
 	return (
 		<>
-			<div className="relative transition-all duration-200 text-card-foreground episode-card-wrapper-dark p-0 w-full overflow-y-scroll z-1 grid grid-cols-1 md:grid-cols-2 gap-5 h-full max-h-[300px]">
+			<div className="relative transition-all duration-200 text-card-foreground episode-card-wrapper-dark p-0 w-full overflow-y-scroll z-1 grid grid-cols-1 md:grid-cols-2 gap-4 h-full max-h-[300px]">
 				{bundles.map(bundle => (
 					<Card
 						key={bundle.bundle_id}
-						className="h-auto flex flex-col px-4 episode-card shadow-lg h-[220px] w-full cursor-pointer hover:shadow-xl transition-shadow duration-200"
+						className="h-auto flex flex-col px-8 rounded-2xl shadow-lg bg-[#181D23] h-[220px] border-2 border-[#4A476A] w-full cursor-pointer hover:shadow-xl transition-shadow duration-200 gap-5"
 						onClick={() => handleBundleClick(bundle)}>
-						<CardHeader className="w-full py-4">
-							<div className="w-full flex flex-row gap-3">
+						<CardHeader className="w-full py-4 px-2">
+							<div className="w-full flex flex-row gap-6">
 								<div className="flex items-start gap-3 text-sm font-normal tracking-wide flex-col w-full max-w-[240px]">
 									<H3 className="text-[1rem] text-secondary-foreground font-black font-sans mt-2 mb-3 leading-9 text-shadow-sm tracking-tight uppercase leading-tight mb-0 truncate">{bundle.name}</H3>
 
@@ -157,12 +157,12 @@ export function CuratedBundlesClient({ bundles, error }: CuratedBundlesClientPro
 										<Typography className="text-xxs">Fixed Podcast Shows</Typography>
 									</Badge>
 
-									<CardContent className="bg-cardglass mx-auto shadow-sm rounded-md w-full p-3 m-0 pb-8 mt-2">
-										<ul className="list-none p-0 m-0 flex flex-col gap-1.5">
-											{bundle.podcasts.slice(1).map((podcast: Podcast) => (
+									<CardContent className="bg-[#353645] mx-auto shadow-sm rounded-md w-full p-2 m-0 pb-3 mt-2 outline-1 outline-[#63758AB7]">
+										<ul className="list-none p-0 m-0 flex flex-col gap-2">
+											{bundle.podcasts?.map((podcast: Podcast) => (
 												<li key={podcast.podcast_id} className="flex w-full justify-end gap-0">
 													<div className="w-full flex flex-col gap-0">
-														<Typography as="p" className="text-md font-bold uppercase leading-normal tracking-tight my-0 px-1 mx-0 opacity-80">
+														<Typography as="p" className="text-md font-bold leading-normal tracking-tight my-0 px-1 mx-0 opacity-80">
 															{podcast.name}
 														</Typography>
 													</div>
@@ -172,8 +172,8 @@ export function CuratedBundlesClient({ bundles, error }: CuratedBundlesClientPro
 									</CardContent>
 								</div>
 
-								<div className="flex items-center gap-2 text-sm font-normal tracking-wide w-full">
-									<div className="relative my-2 rounded-lg outline-4 overflow-hidden w-full min-w-[110px] h-34">
+								<div className="flex items-start gap-2 text-sm font-normal tracking-wide w-full">
+									<div className="relative my-2 rounded-lg outline-4 overflow-hidden w-full min-w-[110px] h-32">
 										{bundle.image_url && <Image className="w-full object-cover" src={bundle.image_url} alt={bundle.name} width={190} height={110} />}
 									</div>
 								</div>
