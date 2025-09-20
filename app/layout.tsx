c07addb4ff7037493adf76fe2b31395cc16a9524
Jaco Kotzee <jakwakwa@gmail.com>
2025-09-21 01:09:09 +0200

    Add @react-email/components package and enhance email template for user invitations
    - Introduced the @react-email/components package to facilitate email rendering.
    - Created a new VercelInviteUserEmail component for sending user invitation emails with improved layout and styling.
    - Updated global CSS styles for better responsiveness and visual consistency across components.
    - Adjusted layout and appearance of various UI elements to enhance user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+import { shadcn } from "@clerk/themes";
 
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
@@ -54,67 +54,12 @@ export default function RootLayout({
 				<ClerkProvider
 					publishableKey={clerkPublishableKey || ""}
 					appearance={{
-						baseTheme: dark, // <-- Move baseTheme here
-						elements: {
-							rootBox: {
-								width: "90vw",
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "center",
-								maxWidth: "500px",
-								minWidth: "700px",
-							},
-							variables: {
-								fontWeight: {
-									normal: 200,
-									medium: 300,
-									semibold: 600,
-									bold: 700,
-								},
-							},
-							socialButtons: {
-								display: "flex",
-								justifyContent: "center",
-								gap: "0.5rem",
-							},
-							button: {
-								display: "flex",
-								alignSelf: "center",
-								justifyContent: "center",
-							},
-							form: {
-								display: "flex",
-								justifyContent: "center",
-							},
-							cardBox: {
-								background: "#121928 !important",
-								width: "90vw",
-								display: "flex",
-								flexDirection: "column",
-								justifyContent: "center",
-								margin: "0 auto",
-								maxWidth: "500px",
-								minWidth: "300px",
-							},
-							card: {
-								background: "#1B1822",
-							},
-							footer: {
-								background: "#354F4C28",
-
-								p: {
-									color: "#37A1A3 !important",
-								},
-							},
-							footerAction: {
-								color: "#121928 !important",
-								p: {
-									color: "#121928 !important",
-								},
-							},
-						},
+						baseTheme: [shadcn],
+						variables: { colorPrimary: "#0AB38E", colorPrimaryForeground: "#000", colorForeground: "#fff", colorInputForeground: "#000000" },
+						signIn: { variables: { colorPrimaryForeground: "#000", colorForeground: "#fff", colorInputForeground: "#000000" } },
 					}}>
-					<ClientProviders>{children}
+					<ClientProviders>
+						{children}
 						<Toaster closeButton richColors />
 						<GlobalAudioPlayerSheet />
 					</ClientProviders>
