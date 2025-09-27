e7f683abdd9d075926e8009037fd82b0b4267112
Jaco Kotzee <jakwakwa@gmail.com>
2025-09-27 20:22:29 +0200

    style: update background styles and enhance visual effects in layout and globals

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -47,11 +47,11 @@ function ProtectedLayoutInner({ children }: { children: React.ReactNode }) {
 
 				<div
 					className={`flex flex-col flex-grow transition-all duration-300 ease-in-out px-0 md:px-0 mt-8 md:mt-0 mb-2 m-0 p-0 h-screen ${state === "expanded" ? "ml-0 w-full md:ml-3 md:pr-2 " : "ml-0 md:ml-12 w-full md:max-w-[95vw]"}`}>
-					{/* <div className={'grain-blur background-base'} /> */}
+					<div className={'grain-blur background-base'} />
 					<div className={'grain-background background-base'} />
-					<div className={'grid-bg-one background-base'} />
+					{/* <div className={'grid-bg-one background-base'} /> */}
 					<div className={'large-blur background-base'} />
-					<div className={'small-blur background-base  layout-inset-background'} />
+					<div className={'layout-inset-background'} />
 					<div className="w-[99%] p-0 flex flex-col my-0 md:flex-row pt-6 md:pb-2 md:pt-20 mx-0 pl-1 pr-3 md:px-2 min-w-full md:my-2 sm:ml-6 md:pl-0 md:pr-12">{children}</div>
 				</div>
 			</SidebarInset>
