7020fc8c595c18b3a879ccf097b0391ca7c80ba0
Jaco Kotzee <jakwakwa@gmail.com>
2025-09-27 13:54:05 +0200

    fix: clean up import statements and remove unused components in SignInPage

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,24 +1,16 @@
-import { SignIn } from "@clerk/nextjs"
+import { SignIn } from "@clerk/nextjs";
 import "@/styles/login.css";
-import { LandingPageHeader } from "@/components/layout/LandingPageHeader";
-
 
 export default function SignInPage() {
 	return (
-
 		<>
 			{/* <div className="container login-background-base" /> */}
 			<div className="grid-bg background-base heroSection" />
 			{/* <div className="large-blur background-base" /> */}
 			{/* <div className="background-overlay" /> */}
-			<div className="container flex flex-col items-center justify-center  min-h-screen  bg-sidebar relative z-10"
-
-			>
-				<LandingPageHeader />
+			<div className="container flex flex-col items-center justify-center  min-h-screen  bg-sidebar relative z-10">
 				<SignIn />
 			</div>
-
 		</>
-
-	)
+	);
 }
